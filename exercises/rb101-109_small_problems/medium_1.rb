25932352bcaf99e42c04d9714263e52ae2473083
melinda-hl-lim <melinda.hl.lim@gmail.com>
2019-11-10 17:32:40 -0800

    Work on medium 1 problems

--- exercises/rb101-109_small_problems/medium_1.rb
+++ exercises/rb101-109_small_problems/medium_1.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 #####################
 # Rotation (Part 1) #
 #####################
@@ -126,14 +128,116 @@ end
 # end
 
 # Test cases
-p max_rotation(735291) == 321579
-p max_rotation(3) == 3
-p max_rotation(35) == 53
-p max_rotation(105) == 15 # the leading zero gets dropped
-p max_rotation(8_703_529_146) == 7_321_609_845
+max_rotation(735291) == 321579
+max_rotation(3) == 3
+max_rotation(35) == 53
+max_rotation(105) == 15 # the leading zero gets dropped
+max_rotation(8_703_529_146) == 7_321_609_845
 
 # Further Exploration
 
 # Assume you do not have the rotate_rightmost_digits or rotate_array methods. How would you approach this problem? Would your solution look different? Does this 3 part approach make the problem easier to understand or harder?
 
 # There is an edge case in our problem when the number passed in as the argument has multiple consecutive zeros. Can you create a solution that preserves zeros?
+
+
+
+###############
+# 1000 Lights #
+###############
+
+# You have a bank of switches before you, numbered from 1 to n. Each switch is connected to exactly one light that is initially off. You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
+
+# Write a method that takes one argument, the total number of switches, and returns an Array that identifies which lights are on after n repetitions.
+
+# input: n, an integer representing iterations and number of lights
+def thousand_lights_hash(n)
+  # Make row of lights
+  lights = Hash.new
+  n.times { |n| lights[n+1] = 'off' }
+
+  # Iterate through n times to turn lights on and off. At end of loop, lights are in their final state.
+  counter = 1
+  while counter <= n
+    lights.each do |key, value|
+      if key % counter == 0
+        lights[key] = ( value == 'off' ? 'on' : 'off' )
+      end
+    end
+    counter += 1
+  end
+
+  # Find lights that are on and return an array of light numbers
+  lights.select { |key, value| value == 'on' }.keys
+end
+
+def thousand_lights_array(n)
+
+end
+
+# Test cases
+thousand_lights_hash(5) # => [1, 4]
+thousand_lights_hash(10) # => [1, 4, 9]
+thousand_lights_hash(1000)
+
+
+
+###############
+# Diamonds!!! #
+###############
+
+# Write a method that displays a 4-pointed diamond in an n x n grid, where n is an odd integer that is supplied as an argument to the method. You may assume that the argument will always be an odd integer.
+
+# Input: n, the dimensions of the grid, the number of rows in the diamond
+# Output: none...? Just display/print diamond to console
+
+# PSEUDOCODE
+# Create array of odd numbers up to n
+# Duplicate array, reverse order of elements, and remove the first element (which should be n).
+# Concatenate the two arrays together to create diamond array
+# For each element, elem, in diamond array:
+#   Create string with elem number of *
+#   Center the string across n characters
+#   Output string to console
+
+def diamond(n)
+  odd_numbers = retrieve_odd_numbers(n)
+  diamond_array = odd_numbers + odd_numbers.reverse[1..-1]
+  diamond_array.each do |num|
+    row_string = ('*' * num).center(n)
+    puts row_string
+  end
+end
+
+
+def retrieve_odd_numbers(n)
+  odd_numbers = []
+  n.times { |n| odd_numbers << (n+1) unless (n+1) % 2 == 0 }
+  odd_numbers
+end
+
+# Launch School Solution
+# def print_row(grid_size, distance_from_center)
+#   number_of_stars = grid_size - 2 * distance_from_center
+#   stars = '*' * number_of_stars
+#   puts stars.center(grid_size)
+# end
+
+# def diamond(grid_size)
+#   max_distance = (grid_size - 1) / 2
+#   max_distance.downto(0) { |distance| print_row(grid_size, distance) }
+#   1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
+# end
+
+# Further Exploration
+
+# Try modifying your solution or our solution so it prints only the outline of the diamond:
+#   *
+#  * *
+# *   *
+#  * *
+#   *
+
+diamond(3)
+diamond(5)
+diamond(99)
