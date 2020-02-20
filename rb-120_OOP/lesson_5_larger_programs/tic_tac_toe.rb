e29fbcce8e8cebd7f0da52d56d9419f88555ec7c
Melinda <melinda.hl.lim@gmail.com>
2020-02-20 09:01:03 -0800

    Add defensive AI for tic tac toe

--- rb-120_OOP/lesson_5_larger_programs/tic_tac_toe.rb
+++ rb-120_OOP/lesson_5_larger_programs/tic_tac_toe.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Board
+  attr_reader :squares
+
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                   [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -19,7 +21,7 @@ class Board
   end
 
   def unmarked_keys
-    @squares.keys.select { |k, v| @squares[k].unmarked? }
+    @squares.keys.select { |k, _| @squares[k].unmarked? }
   end
 
   def full?
@@ -30,18 +32,28 @@ class Board
     !!winning_marker
   end
 
-  def three_identical_markers?(squares)
-    markers = squares.select(&:marked?).collect(&:marker)
-    return false if markers.size != 3
-    markers.min == markers.max
+  def markers_in_line(line)
+    squares.values_at(*line).collect(&:marker)
+  end
+
+  def two_in_a_line?(line, marker)
+    markers = markers_in_line(line)
+    markers.count(marker) == 2
+  end
+
+  def three_identical_markers?(line)
+    markers_in_line = line.select(&:marked?).collect(&:marker)
+    return false if markers_in_line.size != 3
+
+    markers_in_line.min == markers_in_line.max
   end
 
   # Return winning marker or nil
   def winning_marker
     WINNING_LINES.each do |line|
-      squares_to_check = @squares.values_at(*line)
-      if three_identical_markers?(squares_to_check)
-        return squares_to_check.first.marker
+      squares_in_line = @squares.values_at(*line)
+      if three_identical_markers?(squares_in_line)
+        return squares_in_line.first.marker
       end
     end
     nil
@@ -51,6 +63,7 @@ class Board
     (1..9).each { |key| @squares[key] = Square.new }
   end
 
+  # rubocop:disable Metrics/AbcSize
   def draw
     puts "       |       |"
     puts "   #{@squares[1]}   |   #{@squares[2]}   |   #{@squares[3]}   "
@@ -65,6 +78,7 @@ class Board
     puts "       |       |"
   end
 end
+# rubocop:enable Metrics/AbcSize
 
 class Square
   INITIAL_MARKER = " "
@@ -95,6 +109,40 @@ class Player
   end
 end
 
+class Computer < Player
+  def select_move(board)
+    valid_squares = board.unmarked_keys
+    threat = check_for_threats(board, valid_squares)
+    threat ? threat : valid_squares.sample
+  end
+
+  def check_for_threats(board, valid_squares)
+    Board::WINNING_LINES.each do |line|
+      if board.two_in_a_line?(line, TTTGame::HUMAN_MARKER)
+        markers = board.markers_in_line(line)
+        initial_marker = markers.find_index(Square::INITIAL_MARKER)
+        return line[initial_marker] unless initial_marker.nil?
+      end
+    end
+
+    nil
+  end
+end
+
+class Human < Player
+  def select_move(valid_squares_keys)
+    puts "Please choose a square: (#{valid_squares_keys.join(', ')})"
+    square_num = ""
+    loop do
+      square_num = gets.chomp.to_i
+      break if valid_squares_keys.include?(square_num)
+
+      puts "Sorry, that's not a valid choice."
+    end
+    square_num
+  end
+end
+
 class TTTGame
   HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
@@ -109,10 +157,12 @@ class TTTGame
       loop do
         current_player_moves
         break if board.someone_won? || board.full?
+
         clear_screen_and_display_board if human_turn?
       end
       display_result
       break unless play_again?
+
       reset
       display_play_again_message
     end
@@ -123,8 +173,8 @@ class TTTGame
 
   def initialize
     @board = Board.new
-    @human = Player.new(HUMAN_MARKER)
-    @computer = Player.new(COMPUTER_MARKER)
+    @human = Human.new(HUMAN_MARKER)
+    @computer = Computer.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
   end
 
@@ -154,30 +204,16 @@ class TTTGame
   end
 
   def current_player_moves
+    valid_choices_keys = board.unmarked_keys
     if human_turn?
-      human_moves
+      square_num = human.select_move(valid_choices_keys)
+      board[square_num] = human.marker
       @current_marker = COMPUTER_MARKER
     else
-      computer_moves
+      square_num = computer.select_move(board)
+      board[square_num] = computer.marker
       @current_marker = HUMAN_MARKER
     end
-  end
-
-  def human_moves
-    puts "Choose a square (#{board.unmarked_keys.join(', ')}):"
-    square = ""
-    loop do
-      square = gets.chomp.to_i
-      break if board.unmarked_keys.include?(square)
-      puts "Sorry, that's not a valid choice."
-    end
-
-    board[square] = human.marker
-  end
-
-  def computer_moves
-    num = board.unmarked_keys.sample
-    board[num] = computer.marker
   end
 
   def human_turn?
@@ -203,6 +239,7 @@ class TTTGame
       puts "Would you like to play again? (y/n)"
       answer = gets.chomp.downcase
       break if %(y n).include? answer
+
       puts "Sorry, please answer either 'y' or 'n'."
     end
     answer == 'y'
