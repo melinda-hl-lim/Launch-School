3d9b56e40ea8ebb383637ab214c4242e1f752ba7
Melinda <melinda.hl.lim@gmail.com>
2020-04-15 10:30:57 -0700

    rb-175 todo list project - finished writing edit list functionality

---
+++ rb-175_networked_applications/sinatra_todos/todo.rb
@@ -0,0 +1,85 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# Display all lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Return an error message is list name is invalid; otherwise, returns nil
+def error_for_list_name?(name)
+  if !(1..100).cover? name.size
+    "List name must be 1-100 characters long"
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique"
+  end
+end
+
+# Create new list
+post "/lists" do
+  list_name = params["list_name"].strip
+
+  error = error_for_list_name?(list_name)
+  if error 
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# Show a list
+get "/lists/:id" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :list, layout: :layout
+end
+
+# Edit a list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :edit_list, layout: :layout
+end
+
+# Update a existing list
+post "/lists/:id" do
+  list_name = params["list_name"].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name?(list_name)
+  if error 
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{params[:id]}"
+  end
+end
